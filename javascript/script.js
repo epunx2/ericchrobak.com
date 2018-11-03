147a98839812003540ad4da51954611ed3445d18
Eric Chrobak <reflectioncleaning@gmail.com>
2018-11-02 21:27:28 -0500

    Added videos for both app demos. Added hmtl video pages for demos. Added highlighter around photos when choosing in the work area. Uploaded pocketbook and excursion sites.

--- javascript/script.js
+++ javascript/script.js
@@ -8,14 +8,20 @@ var main = function() {
   });
   $('.nav li').click(function() {
     var category = $(this).attr('class');
-		if(category == "nav-html"){
-      $('.thumbnail').hide();
-      $('.html').show();
-    } else if(category == "nav-javascript"){
-      $('.thumbnail').hide();
-      $('.javascript').show();
+		if(category == "nav-javascript"){
+      $('.thumbnail').removeClass('selected');
+      $('.javascript').addClass('selected');
+    } else if(category == "nav-ruby"){
+      $('.thumbnail').removeClass('selected');
+      $('.ruby').addClass('selected');
+    } else if(category == "nav-zohocreator"){
+      $('.thumbnail').removeClass('selected');
+      $('.zohocreator').addClass('selected');
+    } else if(category == "nav-html"){
+      $('.thumbnail').removeClass('selected');
+      $('.html').addClass('selected');
     } else if(category == "nav-all"){
-      $('.thumbnail').show();
+      $('.thumbnail').removeClass('selected');
     }
     $('.nav li').removeClass('active');
     $(this).addClass('active');
