381dbad05a95ce51ef5982015be792c3740e3d14
Eric Chrobak <eric@ericchrobak.com>
2020-04-13 21:05:31 -0500

    Updated the Repo with files and folder
    Made lots of local changes and those were pushed to website. Now updating Repo

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,37 @@
 var main = function() {
-  $(function() {
-    $(".rslides").responsiveSlides({
-      auto: true,
-      speed: 1000,
-      maxwidth: 800
+  /*$(function() {
+    $(".carousel").carousel({
+
+      interval: 1000,
+      pause: "hover"
     });
-  });
+  });*/
   $('.nav li').click(function() {
     var category = $(this).attr('class');
-		if(category == "nav-javascript"){
-      $('.thumbnail').removeClass('selected');
-      $('.javascript').addClass('selected');
-    } else if(category == "nav-ruby"){
-      $('.thumbnail').removeClass('selected');
-      $('.ruby').addClass('selected');
-    } else if(category == "nav-zohocreator"){
-      $('.thumbnail').removeClass('selected');
-      $('.zohocreator').addClass('selected');
-    } else if(category == "nav-html"){
-      $('.thumbnail').removeClass('selected');
-      $('.html').addClass('selected');
-    } else if(category == "nav-all"){
-      $('.thumbnail').removeClass('selected');
+    $('.thumbnail').removeClass('selected');
+    $('.thumbnail').addClass('unselected');
+    switch (category) {
+      case "nav-javascript":
+        $('.javascript').removeClass('unselected');
+        $('.javascript').addClass('selected');
+        break;
+      case "nav-ruby":
+        $('.ruby').removeClass('unselected');
+        $('.ruby').addClass('selected');
+        break;
+      case "nav-zohocreator":
+        $('.zohocreator').removeClass('unselected');
+        $('.zohocreator').addClass('selected');
+        break;
+      case "nav-html":
+        $('.html').removeClass('unselected');
+        $('.html').addClass('selected');
+        break;
+      default:
+        $('.thumbnail').removeClass('unselected');
+        $('.thumbnail').addClass('selected');
     }
+
     $('.nav li').removeClass('active');
     $(this).addClass('active');
   });
